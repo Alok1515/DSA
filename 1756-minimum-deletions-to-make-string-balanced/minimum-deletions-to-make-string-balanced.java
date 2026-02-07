e4b0dc6455fe2f6e5438ec1afa34a86dcecf4c19
Alok <alokkumardev15@gmail.com>
2026-02-07 20:04:38 +0530

    Time: 38 ms (41.45%) | Memory: 48 MB (30.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -2,31 +2,31 @@ class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
 
-        int[] rightAcount = new int[n + 1];  // a's after index i
-
-
-        // Suffix count of 'a'
-        for (int i = n - 1; i >= 0; i--) {
-            rightAcount[i] = rightAcount[i + 1];
-            if (s.charAt(i) == 'a') {
-                rightAcount[i]++;
-            }
-        }
-
-        // Find minimum deletions
-        int ans = Integer.MAX_VALUE;
-
+        int countA = 0;
         int countB = 0;
 
+        // Count total 'a'
         for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, countB + rightAcount[i]);
-            if(s.charAt(i) == 'b') {
-                countB++;
+            if (s.charAt(i) == 'a') {
+                countA++;
             }
         }
-         // Split after last character
-        ans = Math.min(ans, countB);
 
-        return ans;
+        // Case: delete all 'a'
+        int count = countA;
+
+        // Sweep through string
+        for (int i = 0; i < n; i++) {
+
+            if (s.charAt(i) == 'a') {
+                countA--; // move this 'a' from right to left
+            } else {
+                countB++; // this 'b' is now on left
+            }
+
+            count = Math.min(count, countA + countB);
+        }
+
+        return count;
     }
 }
