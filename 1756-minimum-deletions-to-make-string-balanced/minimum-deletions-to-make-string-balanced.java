093bfcadc3f7e100714ac6e37a796b35e5e8273d
Alok <alokkumardev15@gmail.com>
2026-02-07 19:51:41 +0530

    Time: 43 ms (34.21%) | Memory: 48.1 MB (9.21%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -2,16 +2,8 @@ class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
 
-        int[] leftBcount = new int[n + 1];   // b's before index i
         int[] rightAcount = new int[n + 1];  // a's after index i
 
-        // Prefix count of 'b'
-        for (int i = 1; i <= n; i++) {
-            leftBcount[i] = leftBcount[i - 1];
-            if (s.charAt(i - 1) == 'b') {
-                leftBcount[i]++;
-            }
-        }
 
         // Suffix count of 'a'
         for (int i = n - 1; i >= 0; i--) {
@@ -24,9 +16,16 @@ class Solution {
         // Find minimum deletions
         int ans = Integer.MAX_VALUE;
 
-        for (int i = 0; i <= n; i++) {
-            ans = Math.min(ans, leftBcount[i] + rightAcount[i]);
+        int countB = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = Math.min(ans, countB + rightAcount[i]);
+            if(s.charAt(i) == 'b') {
+                countB++;
+            }
         }
+         // Split after last character
+        ans = Math.min(ans, countB);
 
         return ans;
     }
