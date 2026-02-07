b507322f849100c3998acd240aa58a44e2762929
Alok <alokkumardev15@gmail.com>
2026-02-07 19:39:31 +0530

    Time: 46 ms (23.68%) | Memory: 48 MB (16.28%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -2,20 +2,32 @@ class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
 
-        int minDel = 0;
+        int[] leftBcount = new int[n + 1];   // b's before index i
+        int[] rightAcount = new int[n + 1];  // a's after index i
 
-        Stack<Character> stack = new Stack<>();
-
-        for(int i = 0; i < n; i++) {
-            // that means ba
-            if(!stack.isEmpty() && s.charAt(i) == 'a' && stack.peek() == 'b') {
-                stack.pop();
-                minDel++;
-            } else {
-                stack.push(s.charAt(i));
+        // Prefix count of 'b'
+        for (int i = 1; i <= n; i++) {
+            leftBcount[i] = leftBcount[i - 1];
+            if (s.charAt(i - 1) == 'b') {
+                leftBcount[i]++;
             }
         }
 
-        return minDel;
+        // Suffix count of 'a'
+        for (int i = n - 1; i >= 0; i--) {
+            rightAcount[i] = rightAcount[i + 1];
+            if (s.charAt(i) == 'a') {
+                rightAcount[i]++;
+            }
+        }
+
+        // Find minimum deletions
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= n; i++) {
+            ans = Math.min(ans, leftBcount[i] + rightAcount[i]);
+        }
+
+        return ans;
     }
 }
