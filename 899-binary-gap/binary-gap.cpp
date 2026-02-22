17c60a9c85772abbf714ed30b0191f5f1a814e4f
Alok <alokkumardev15@gmail.com>
2026-02-22 23:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int prev = -1;
+
+        int result = 0;
+
+        for(int curr = 0; curr < 32; curr++) {
+            if(((n >> curr) & 1) > 0) {
+                result = (prev != -1) ? max(result, curr-prev) : result;
+                prev = curr;
+            }
+        }
+
+        return result;
+    }
+};
