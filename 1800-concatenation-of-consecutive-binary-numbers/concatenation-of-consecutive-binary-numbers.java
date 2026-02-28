8a3ffe68158531827671196f40a84a3dabbc7bcc
Alok <alokkumardev15@gmail.com>
2026-02-28 15:16:36 +0530

    Time: 42 ms (48.84%) | Memory: 42.4 MB (84.88%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int M = 1_000_000_007;
+        int digits = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            //digit count tabhi barhta hai jab power of 2 ata hai
+            if ((i & (i - 1)) == 0) {
+                digits++;
+            }
+            
+            result = ((result << digits) % M + i) % M;
+        }
+        
+        return (int) result;
+    }
+}
