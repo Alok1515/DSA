5002a07f0c71e75f806feccad53a8b001352acea
Alok <alokkumardev15@gmail.com>
2026-05-05 06:22:12 +0530

    Time: 357 ms (5.35%) | Memory: 47.9 MB (61.42%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        Boolean[] dp = new Boolean[n];
+        return solve(0, nums, dp);
+    }
+
+    private boolean solve(int idx, int[] nums, Boolean[] dp) {
+
+        if(idx >= nums.length - 1) return true;
+
+        if(dp[idx] != null) return dp[idx];
+
+        for(int i = 1; i <= nums[idx]; i++) {
+            if(solve(idx + i, nums, dp)) {
+                return dp[idx] = true;
+            }
+        }
+
+        return dp[idx] = false;
+    }
+}
