33901bbb8151546544e26bac8ce76e8300b9ba1a
Alok <alokkumardev15@gmail.com>
2026-05-19 17:51:54 +0530

    Time: 1 ms (13.54%) | Memory: 42.5 MB (12.32%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,25 +1,26 @@
 class Solution {
-
-    Integer[][] dp;
-
-    private int solve(int i, int j, int m, int n) {
-
-        // base case
-        if(i == m-1 && j == n-1) return 1;
-
-        if(i > m || j > n) return 0;
-
-        if(dp[i][j] != null) return dp[i][j];
-
-        int right = solve(i, j+1, m, n);
-        int down = solve(i+1, j, m, n);
-
-        return dp[i][j] = right + down; // total no. of ways
-    }
     public int uniquePaths(int m, int n) {
 
-        dp = new Integer[m+1][n+1];
-        
-        return solve(0, 0, m, n);
+        int[][] dp = new int[m+1][n+1];
+
+        // base case
+        dp[m-1][n-1] = 1;
+
+
+        for(int i = m-1; i >= 0; i--) {
+
+            for(int j = n-1; j >= 0; j--) {
+
+                // skip destination cell
+                if(i == m - 1 && j == n - 1) continue;
+
+                int right = (j+1 < n) ? dp[i][j+1] : 0;
+                int down  = (i+1 < m) ? dp[i+1][j] : 0;
+
+                dp[i][j] = right + down;
+            }
+        }
+
+        return dp[0][0];
     }
 }
