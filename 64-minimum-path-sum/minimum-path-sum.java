ec1c0a7095d8324323054ccc1101177448b9066c
Alok <alokkumardev15@gmail.com>
2026-05-20 17:18:44 +0530

    Time: 9 ms (10.14%) | Memory: 49.8 MB (93.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,31 +1,29 @@
 class Solution {
-
-    int m, n;
-    Integer[][] dp;
-
-    private int solve(int i, int j, int[][] grid) {
-
-        if(i >= m || j >= n) return Integer.MAX_VALUE;
-
-        // base case
-        if(i == m-1 && j == n-1) {
-            return grid[i][j];
-        }
-
-        if(dp[i][j] != null) return dp[i][j];
-
-        int down  =  solve(i+1, j, grid);
-        int right =  solve(i, j+1, grid);
-
-        return dp[i][j] = grid[i][j] + Math.min(down, right);
-    }
     public int minPathSum(int[][] grid) {
         
-        m = grid.length;
-        n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        dp = new Integer[m][n];
+        Integer[][] dp = new Integer[m+1][n+1];
 
-        return solve(0, 0, grid);
+        dp[m-1][n-1] = grid[m-1][n-1];
+
+        for(int i = m-1; i >= 0; i--) {
+
+            for(int j = n-1; j >= 0; j--) {
+
+                // skip destination cell
+                if(i == m - 1 && j == n - 1) {
+                    continue;
+                }
+
+                int down  = (i+1) < m ? dp[i+1][j] : Integer.MAX_VALUE;
+                int right = (j+1) < n ? dp[i][j+1] : Integer.MAX_VALUE;
+
+                dp[i][j] = grid[i][j] + Math.min(down, right);
+            }
+        }
+
+        return dp[0][0];
     }
 }
