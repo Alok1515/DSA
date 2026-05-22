82d5d3849109c3918ebfc84f0d6b942bfa034cdb
Alok <alokkumardev15@gmail.com>
2026-05-22 07:06:59 +0530

    Time: 8 ms (31.19%) | Memory: 47.4 MB (31.26%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,34 +1,30 @@
 class Solution {
-
-    int m, n;
-    Integer[][] dp;
-
-    private int solve(int i, int j, int[][] matrix) {
-
-        if(j < 0 || j >= n) return Integer.MAX_VALUE;
-
-        // base case
-        if(i == m-1) return matrix[i][j];
-
-        if(dp[i][j] != null) return dp[i][j];
-
-        int down      = solve(i+1, j, matrix);
-        int diagLeft  = solve(i + 1, j - 1, matrix);
-        int diagRight = solve(i + 1, j + 1, matrix);
-
-        return dp[i][j] = matrix[i][j] + Math.min(down, Math.min(diagLeft, diagRight));
-    }
+    
     public int minFallingPathSum(int[][] matrix) {
         
-        m = matrix.length;
-        n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        dp = new Integer[m+1][n+1];
-
-        int ans = Integer.MAX_VALUE;
+        int[][] dp = new int[m+1][n+1];
 
         for(int j = 0; j < n; j++) {
-            ans = Math.min(ans, solve(0, j, matrix));
+            dp[m-1][j] = matrix[m-1][j];
+        }
+
+        for(int i = m-2; i >= 0; i--) {
+            for(int j = 0; j < n; j++) {
+
+                int down =  dp[i+1][j];
+                int diagLeft = (j-1 >= 0) ? dp[i+1][j-1] : Integer.MAX_VALUE;
+                int diagRight = (j+1 < n) ? dp[i+1][j+1] : Integer.MAX_VALUE;
+
+                dp[i][j] = matrix[i][j] + Math.min(down, Math.min(diagLeft, diagRight));
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int j = 0; j < n; j++) {
+            ans = Math.min(ans, dp[0][j]);
         }
 
         return ans;
