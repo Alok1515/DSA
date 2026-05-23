b47dc783bfc2ed0ce8009afbf4276b53aaeec714
Alok <alokkumardev15@gmail.com>
2026-05-23 12:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (95.07%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,55 +1,32 @@
 class Solution {
-
-    private int findPivot(int[] nums, int n) {
-
-        int l = 0, r = n-1;
-
-        while(l < r) {
-
-            int mid = l + (r - l) / 2;
-
-            if(nums[mid] > nums[r]) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
-        }
-
-        return r; //pivot index
-    }
-
-    private int binarySearch(int l, int r, int[] nums, int target) {
-
-        int idx = -1;
+    public int search(int[] nums, int target) {
+        
+        int l = 0, r = nums.length-1;
 
         while(l <= r) {
 
             int mid = l + (r - l) / 2;
 
-            if(nums[mid] == target) {
-                idx = mid;
-                break;
-            } else if(nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+            if(nums[mid] == target) return mid;
+
+            // left sorted half
+            if(nums[l] <= nums[mid]) {
+
+                // check if it is in left side
+                if(nums[l] <= target && target <= nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else { // right sorted half
+                if(nums[mid] <= target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
 
-        return idx;
-    }
-    public int search(int[] nums, int target) {
-        
-        int n = nums.length;
-
-        int pivotIndex = findPivot(nums, n);
-
-        int index = binarySearch(0, pivotIndex - 1, nums, target); // left side of pivot index
-
-        if(index != -1) return index;
-
-        index = binarySearch(pivotIndex, n - 1, nums, target);
-
-        return index;
+        return -1;
     }
 }
