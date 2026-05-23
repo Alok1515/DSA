fda3a17ab51a4ce3aa12abccadc17ee03bd4adc6
Alok <alokkumardev15@gmail.com>
2026-05-23 12:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (79.85%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -20,13 +20,13 @@ class Solution {
             if(nums[l] <= nums[mid]) {
 
                 // check if it is in left side
-                if(nums[l] <= target && target < nums[mid]) {
+                if(nums[l] <= target && target <= nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
             } else { // right sorted half
-                if(nums[mid] < target && target <= nums[r]) {
+                if(nums[mid] <= target && target <= nums[r]) {
                     l = mid + 1;
                 } else {
                     r = mid - 1;
