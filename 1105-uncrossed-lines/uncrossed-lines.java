190151a5261f081e4e59793d2541225dcec5b78d
Alok <alokkumardev15@gmail.com>
2026-05-28 16:06:15 +0530

    Time: 6 ms (81.55%) | Memory: 45.4 MB (82.97%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -13,7 +13,7 @@ class Solution {
         if(dp[i][j] != null) return dp[i][j];
 
         if(nums1[i] == nums2[j]) {
-            return 1 + solve(i+1, j+1, nums1, nums2);
+            return dp[i][j] = 1 + solve(i+1, j+1, nums1, nums2);
         }
 
         // otherwise skip one element
