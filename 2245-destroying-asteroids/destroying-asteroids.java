36bcdff475aab06207b435fbf3a87c0295ae56b2
Alok <alokkumardev15@gmail.com>
2026-05-31 16:56:44 +0530

    Time: 25 ms (93.30%) | Memory: 107.4 MB (47.81%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+
+        int n = asteroids.length;
+
+        Arrays.sort(asteroids);
+
+        long currMass = mass;
+
+        for(int asteroid : asteroids) {
+            if(asteroid > currMass) return false;
+
+            currMass += asteroid;
+        }
+
+        return true;
+    }
+}
