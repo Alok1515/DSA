2a1a14ae702e76bfd54160c9d769e8e1bb49cc8e
Alok <alokkumardev15@gmail.com>
2026-06-16 13:36:23 +0530

    Time: 5 ms (18.09%) | Memory: 46.4 MB (19.23%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,14 +1,26 @@
 class Solution {
 
-    private boolean hasCycle(int src, int dest, List<Integer>[] graph, boolean[] visited) {
+    int n;
 
-        if(src == dest) return true;
+    private boolean hasCycle(int src, int dest, List<Integer>[] graph) {
 
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n+ 1];
+
+        q.offer(src);
         visited[src] = true;
 
-        for(int neighbor : graph[src]) {
-            if(!visited[neighbor]) {
-               if( hasCycle(neighbor, dest, graph, visited)) return true;
+        while(!q.isEmpty()) {
+
+            int curr = q.poll();
+
+            if(curr == dest) return true;
+
+            for(int nei : graph[curr]) {
+                if(!visited[nei]) {
+                    q.offer(nei);
+                    visited[nei] = true;
+                }
             }
         }
 
@@ -16,7 +28,7 @@ class Solution {
     }
     public int[] findRedundantConnection(int[][] edges) {
         
-        int n = edges.length;
+        n = edges.length;
 
         List<Integer>[] graph = new ArrayList[n+1];
         for(int i = 0; i <= n; i++) {
@@ -27,9 +39,7 @@ class Solution {
             int u = edge[0];
             int v = edge[1];
 
-            boolean[] visited = new boolean[n+1];
-
-            if(!graph[u].isEmpty() && !graph[v].isEmpty() && hasCycle(u, v, graph, visited)) {
+            if(!graph[u].isEmpty() && !graph[v].isEmpty() && hasCycle(u, v, graph)) {
                 return edge;
             }
             graph[u].add(v);
